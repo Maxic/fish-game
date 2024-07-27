e40b4c594c199c9dd2c39864ddb3cc292bc5a832
Maxic <barend.poot@gmail.com>
2024-07-27 14:10:02 +0200

    hook line and sinker

--- scripts/hook.gd
+++ scripts/hook.gd
@@ -4,7 +4,7 @@ extends Node2D
 @export var speed: float = 70.0
 var direction: Vector2 = Vector2(1,1)
 
-const hook_fallback_lerp_weight = .03
+const hook_fallback_lerp_weight = .02
 
 func _physics_process(delta):
 	
@@ -15,16 +15,18 @@ func _physics_process(delta):
 	
 	direction = direction.normalized() * speed
 	
+	var sprite_rot = $Sprite2D.rotation
+	
 	# Rotate as if being dragged
 	if abs(direction.x) < .01 and abs(direction.y) <.01:
 		var look_at_point = Vector2(self.global_position.x, self.global_position.y+1)
-		rotation = lerp_angle(rotation, rotation + get_angle_to(look_at_point), hook_fallback_lerp_weight)
+		$Sprite2D.rotation = lerp_angle($Sprite2D.rotation, rotation + get_angle_to(look_at_point)-PI/2, hook_fallback_lerp_weight)
 	else:
 		var look_at_point = self.global_position +(direction*-1)
 		# don't rotate when going down, since we're already dropping down
 		if look_at_point.y < self.global_position.y:
 			look_at_point.y = self.global_position.y +2
-		rotation = lerp_angle(rotation, rotation + get_angle_to(look_at_point), hook_fallback_lerp_weight)
+		$Sprite2D.rotation = lerp_angle($Sprite2D.rotation, rotation + get_angle_to(look_at_point)-PI/2, hook_fallback_lerp_weight)
 
 	translate(direction * delta)
 	
--- scripts/line.gd
+++ scripts/line.gd
@@ -2,12 +2,17 @@ extends Line2D
 
 var global_line_origin: Vector2
 @onready var window_size = get_window().size
+@onready var sprite_position = get_node("../Sprite2D").position
+var points_amount = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global_line_origin = Vector2(window_size.x/2, 0)
-	add_point(to_local(global_line_origin))
-	add_point(Vector2(-2,0))
+	for i in range(0, points_amount):
+		add_point(to_local(global_line_origin))
+	points[points_amount-1] = sprite_position
+	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -15,12 +20,25 @@ func _physics_process(delta):
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		0	
 	)
-	var new_x_pos 
 	
+	# Get new origin point
+	var new_x_pos: float 
 	if direction.x == 0:
 		new_x_pos = lerp(global_line_origin.x, (self.global_position).x, .05)
 	else:
 		new_x_pos = global_line_origin.x + (direction.normalized().x * 3)
 	
+	# Clamp origin so it doesnt go all the way out there
 	global_line_origin.x = clamp(new_x_pos, 0, window_size.x)
-	points[0] = to_local(global_line_origin)
+	
+	var weight: float = 1
+	var offset: float = .005
+	var weight_step: float = weight/(points_amount-1)
+	
+	for i in range(0, points_amount):
+		var straight_line_point = to_local(to_global(points[points.size()-1]).lerp(global_line_origin, weight))
+		points[i] = Vector2(straight_line_point.x*(i*offset+1)*(i*offset+1), straight_line_point.y)
+		weight -= weight_step
+	
+	points[points_amount-1] = to_local(to_global(Vector2(sprite_position.x, sprite_position.y)))
+	
